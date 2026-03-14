4837c0cf570d1010e1a7c9bb8e9ea3287f39d181
joe-cena-git <joecenatiempo@gmail.com>
2026-03-14 11:16:23 -0400

    Largest number in vector calculations

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,73 @@
 fn main() {
 
-    let number_list = vec![32, 50, 25, 100, 65];
+    // 10-1 Finding the largest number in a list of numbers ----
+    {
+        let number_list = vec![32, 50, 25, 100, 65];
 
-    let mut largest = &number_list[0];
+        let mut largest = &number_list[0];
 
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+        for number in &number_list {
+            if number > largest {
+                largest = number;
+            }
         }
+
+        println!("The largest number is {largest}");
     }
 
-    println!("The largest number is {largest}");
+    // 10-2 Code to find the largest number in two lists of numbers ----
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let mut largest = &number_list[0];
+
+        for number in &number_list {
+            if number > largest {
+                largest = number;
+            }
+        }
+
+        println!("The largest number is {largest}");
+
+        let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+        let mut largest = &number_list[0];
+
+        for number in &number_list {
+            if number > largest {
+                largest = number;
+            }
+        }
+
+        println!("The largest number is {largest}");
+    }
+
+    // 10-3 Abstracted code to find the largest number in two lists ----
+    {
+        fn largest(list:&[i32]) -> &i32 {
+            let mut largest = &list[0];
+
+            for item in list {
+                if item > largest {
+                    largest = item;
+                }
+            }
+
+            largest
+        }
+
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let result = largest(&number_list);
+        println!("The largest number is {result}");
+
+        let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+        let result = largest(&number_list);
+        println!("The largest number is {result}");
+    }
+
+    
+
+
 }
